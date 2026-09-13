a9740dabfe1a501f692fd66e7061abf4b0d292c9
Ahmed <ahmed@gmail.com>
2026-09-13 18:39:46 +0300

    scrol pages

--- src/Pages/Home/RootLayout.jsx
+++ src/Pages/Home/RootLayout.jsx
@@ -1,10 +1,12 @@
 import { NavBarLogin } from "../../Components/utils/NavbarLogin";
 import { Footer } from "../../Components/utils/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../../Components/utils/ScrollToTop";
 
 export const RootLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBarLogin />
       <Outlet />
       <Footer />
